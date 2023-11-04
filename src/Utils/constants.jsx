99ddb6a892527ed68a63b567445e5e79dace23d4
suleman-084 <sallusk084@gmail.com>
2023-11-04 19:50:41 +0530

    implemented sign out and updated profile api call

--- src/Utils/constants.jsx
+++ src/Utils/constants.jsx
@@ -1,3 +1,5 @@
 export const  logo = "https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png";
 
 export const background_img = "https://assets.nflxext.com/ffe/siteui/vlv3/dace47b4-a5cb-4368-80fe-c26f3e77d540/f5b52435-458f-498f-9d1d-ccd4f1af9913/IN-en-20231023-popsignuptwoweeks-perspective_alpha_website_large.jpg";
+
+export const PROFILE_PICTURE = "https://media.licdn.com/dms/image/D5603AQHnxXi0WE8oww/profile-displayphoto-shrink_800_800/0/167699454767https://media.licdn.com/dms/image/D5603AQHnxXi0WE8oww/profile-displayphoto-shrink_800_800/0/1676994547678?e=1704326400&v=beta&t=HqpfFtIoncowx1F2z9Pf6maAjMfeP5CzP1eis3HRooo8?e=1704326400&v=beta&t=https://media.licdn.com/dms/image/D5603AQHnxXi0WE8oww/profile-displayphoto-shrink_800_800/0/1676994547678?e=1704326400&v=beta&t=HqpfFtIoncowx1F2z9Pf6maAjMfeP5CzP1eis3HRooo"
