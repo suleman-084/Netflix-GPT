426b303d52fb35ed8d1d21bd3df3f26546db7d41
suleman-084 <sallusk084@gmail.com>
2023-11-20 17:20:23 +0530

    created a custom hook to make code modular

---
+++ src/hooks/useMovieTrailer.jsx
@@ -0,0 +1,28 @@
+import { useEffect } from 'react'
+import { useDispatch } from 'react-redux'
+import { API_OPTIONS } from '../Utils/constants'
+import { addTrailerVideo } from '../Utils/moviesSlice'
+
+const useMovieTrailer = (movieId) => {
+    const dispatch = useDispatch()
+    const getMovieVideos = async () => {
+        const data = await fetch("https://api.themoviedb.org/3/movie/" + movieId + "/videos?language=en-US", API_OPTIONS)
+
+        const json = await data.json()
+        console.log("trailer", json);
+
+        const filterData = json.results.filter((video) => video.type === "Trailer"
+        )
+        const trailer = filterData.length ? filterData[0] : json.results[0]
+        console.log("trailer", trailer);
+        dispatch(addTrailerVideo(trailer))
+
+    }
+    useEffect(() => {
+        getMovieVideos()
+
+    }, [])
+
+}
+
+export default useMovieTrailer
